50464ddd02412c099ec8ad77e5b247e309d2500e
Hoshibi <tanyshiotani@hotmail.com>
2022-03-29 11:05:15 -0300

    Component ImageBox
    - Criação do component ImageBox
    - Estilização do component ImageBox
    - Adição da Imagem utilizada do component ImageBox

---
+++ src/components/ImageBox/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+const ImageBox: React.FC = () => {
+    return (
+        <Container>
+            <img src="./GirlImage.png" alt="Girl_Image"/>
+        </Container>
+    )
+}
+
+export default ImageBox;
